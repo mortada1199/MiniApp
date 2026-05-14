b7ecfe1619260ae406b025fb5d589bb4afe050a8
mortada1199 <murtada199815@gmail.com>
2026-05-14 11:54:54 +0200

    add

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -24,7 +24,7 @@ class AuthController extends Controller
         ]);
 
         $response = Http::post(
-            'https://tyjtp-196-1-227-87.run.pinggy-free.link/tma/authenticate',
+            'https://gdnpx-196-1-227-87.run.pinggy-free.link/tma/authenticate',
             [
                 'username' => $request->username,
                 'pin' => $request->pin,
