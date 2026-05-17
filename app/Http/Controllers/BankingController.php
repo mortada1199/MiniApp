d900abb3445b63b0abb263ea3e94e03bbb612e81
mortada1199 <murtada199815@gmail.com>
2026-05-17 09:36:47 +0200

    add

--- app/Http/Controllers/BankingController.php
+++ app/Http/Controllers/BankingController.php
@@ -31,7 +31,7 @@ class BankingController extends Controller
         $token = Session::get('token');
         $response = Http::withToken($token)
             ->post(
-                'https://ougog-196-1-227-87.run.pinggy-free.link/tma/transfer',
+                'https://sszck-196-1-227-87.run.pinggy-free.link/tma/transfer',
                 [
                     'accountFrom' => $request->accountFrom,
                     'accountTo' => $request->accountTo,
