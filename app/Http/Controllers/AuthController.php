086d3d648aeea24e6ccdadbd82b50dcf2c66eb3e
mortada1199 <murtada199815@gmail.com>
2026-05-18 08:30:03 +0200

    add

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -24,7 +24,7 @@ class AuthController extends Controller
         ]);
 
         $response = Http::post(
-            'https://qetwr-196-1-227-87.run.pinggy-free.link/tma/authenticate',
+            'https://dprur-196-1-227-87.run.pinggy-free.link/tma/authenticate',
             [
                 'username' => $request->username,
                 'pin' => $request->pin,
